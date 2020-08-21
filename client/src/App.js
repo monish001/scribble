be919f4630fd7f1ebdc964eaf1ac65d49b873809
Monish Gupta <m0g01ts@homeoffice.wal-mart.com>
2020-08-22 01:23:46 +0530

    basic fe and be setup done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [test, setTest] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/users')
+      .then(results => results.json())
+      .then(data => setTest(JSON.stringify(data)));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {test}
         </p>
         <a
           className="App-link"

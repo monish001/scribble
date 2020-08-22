7cc69f617524f81f74a70485a4eb10b720560a9b
Monish Gupta <m0g01ts@homeoffice.wal-mart.com>
2020-08-23 03:13:20 +0530

    server uptime more readable now

--- server/routes/common.js
+++ server/routes/common.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/health', function (req, res, next) {
-  res.send({ uptime: process.uptime() });
+
+  res.send({ uptime: `Server up since ${new Date(new Date().getTime() - process.uptime() * 1000)}` });
 });
 
 module.exports = router;
